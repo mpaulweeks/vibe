2741da624eec826e9c58392909a47ebc2c09c96a
mpaulweeks <mpaulweeks@gmail.com>
2018-03-26 18:41:55 -0400

    delete unused boilerplate, get cube working

--- vibe-app/src/App.js
+++ vibe-app/src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import Canvas from './Canvas';
+import Brain from './Brain';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.brain = new Brain(this.refs.canvas.elm);
+    window.brain = this.brain;
+    console.log('mounted App', this.brain);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Canvas ref="canvas"></Canvas>
     );
   }
 }
---
+++ vibe-app/src/Canvas.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const CanvasContainer = styled.div`
+    cursor: none;
+    height: 100vh;
+`;
+
+const CanvasElm = styled.canvas`
+`;
+
+class Canvas extends React.Component {
+  render() {
+    return (
+      <CanvasContainer>
+        <CanvasElm innerRef={e => this.elm = e}></CanvasElm>
+      </CanvasContainer>
+    );
+  }
+}
+
+export default Canvas;
